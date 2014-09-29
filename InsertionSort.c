49c1faafaa764942459a01a6bf9a4c34f4620c73
William Brownson <Williamiv95@gmail.com>
2014-09-29 00:37:32 -0700

    WOOHOO! ... now to the actual Assingment
    Able to read the file and put each line as an item in the list.

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,23 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int read_line(){
-	FILE *source;
-	source = fopen(source,"r");
-	int line;
-	fscanf(source, "%d", &line);
-	return line;
-};
-
 int main() {
 	int sorted[64];
 	int i;
+	FILE * source;
+	source = fopen("Example.txt","r");
 	for (i=0 ; i < 64 ; i++){
-		sorted[i] = 0;
+		fscanf(source, "%d", &sorted[i]);
 	}
 	for (i=0 ; i < 64 ; i++){
-		printf("%d, ", sorted[i]);
+		printf("%d\n", sorted[i]);
 	}
-	/*for (i=0 ; current != * ; i++) {
-		*/
 }
